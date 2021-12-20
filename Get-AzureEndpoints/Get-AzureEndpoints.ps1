d17ab1db3cd42602fb719de1b69a872a13a6d780
Pit Singert <75387082+pisinger@users.noreply.github.com>
2021-12-20 14:56:02 +0100

    removed alias commands

--- Get-AzureEndpoints/Get-AzureEndpoints.ps1
+++ Get-AzureEndpoints/Get-AzureEndpoints.ps1
@@ -18,6 +18,9 @@
 	Get-AzureEndpoints -IPv4 | where Region -like "*ger*" | where Name -like "sql" | ft
 	Get-AzureEndpoints -IPv4 | where Region -like "*ger*"| select Subnets -Unique
 	Get-AzureEndpoints -IPv4 | where Name -like *ThreatProtection* | ft
+	Get-AzureEndpoints -IPv4 | group region | sort Count -Descending
+	Get-AzureEndpoints -IPv4 | where Region -like "*europe*" | group region
+	Get-AzureEndpoints -IPv4 | where Region -like "*europe*" | group Service | sort Count -Descending
 #>
 
 param (
@@ -36,16 +39,16 @@ IF(Test-Path $LocalCopy){
 ELSE {
 	# download endpoint file
 	$href = Invoke-WebRequest -Method Get -Uri "https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519"
-	$downloadUri = ($href.links | where href -like "*download.microsoft.com*.json" | select -first 1).href
+	$downloadUri = ($href.links | Where-Object href -like "*download.microsoft.com*.json" | Select-Object -first 1).href
 	$fileName = $downloadUri | Split-Path -Leaf
 	Invoke-WebRequest -Method Get -Uri $downloadUri -Outfile $($PSScriptRoot + "\" + $fileName)
 	
 	$json = Get-Content $($PSScriptRoot + "\" + $fileName) | ConvertFrom-Json
 	
-	$results += $json.values| FOREACH {
+	$results += $json.values| ForEach-Object {
 		$p = $_
 			
-		$p.properties.addressPrefixes | FOREACH {			
+		$p.properties.addressPrefixes | ForEach-Object {
 			[PSCustomObject]@{
 				Name = ($p.Name -split "\.",2)[0]
 				SubName = ($p.Name -split "\.",2)[1]
@@ -59,8 +62,8 @@ ELSE {
 	$Endpoints = Import-Csv $LocalCopy
 }
 
-IF ($IPv4) {$Endpoints = $Endpoints | where Subnets -notlike "*:*"}
-ELSEIF ($IPv6) {$Endpoints = $Endpoints | where Subnets -like "*:*"}
+IF ($IPv4) {$Endpoints = $Endpoints | Where-Object Subnets -notlike "*:*"}
+ELSEIF ($IPv6) {$Endpoints = $Endpoints | Where-Object Subnets -like "*:*"}
 ELSE {}
 
 # output
